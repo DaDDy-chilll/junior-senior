b93130f91bfe64d78e1c40cd4dcc511262eba5a1
DaDDy <blacklistuser420@gmail.com>
2023-04-08 12:53:19 +0630

    upadate app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,20 +13,20 @@ class App extends React.Component {
   }
 
   onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
     const filterRobots = this.state.robots.filter((robot) => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
-    console.log(filterRobots);
-  };
-
-  render() {
     return (
       <div className="tc">
         <h1>Robort Frindes</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />;
+        <CardList robots={filterRobots} />;
       </div>
     );
   }
